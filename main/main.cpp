5f84fd1150516fde7b544efedfbb1a0d93dc8801
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2025-05-11 14:02:27 +0000

    edit main

--- main/main.cpp
+++ main/main.cpp
@@ -7,7 +7,7 @@ int num[500000];
 int main() {
   map<int, int, greater<int>> max;
 
-  int size, trade;
+  int size, trade, cnt = 0;
   cin >> size >> trade;
 
   for (int i = 0; i < size; i++) {
@@ -15,33 +15,31 @@ int main() {
     max[num[i]] = i;
   }
 
-  for (auto p : max) {
-    for (int i = p.first - 1; i > 0; i--) {
-      if (p.first != num[i]) {
+  for (auto it = max.begin(); it != max.end(); it++) {
+    auto curr_it = *it;
+
+    for (int i = curr_it.first - 1; i > 0; i--) {
+      if (curr_it.first != num[i]) {
+        auto targ_it = max.find(i);
+
         int temp = num[i];
-        num[i] = p.first;
-        num[p.second] = temp;
+        num[i] = curr_it.first;
+        num[curr_it.second] = temp;
+
+        cnt++;
+        if (trade == cnt) {
+          cout << temp << ' ' << num[i];
+          return 0;
+        }
+
+        temp = curr_it.second;
+        curr_it.second = targ_it->second;
+        targ_it->second = temp;
         break;
       } else
         break;
     }
   }
-
-  for (int i = 0; i < size; i++) {
-    cout << num[i] << ' ';
-  }
+  cout << -1 << '\n';
   return 0;
-
-  /*
-  for (int i = size - 1; i > 0; i--) {
-    for (int j = i; j > 0; j--) {
-      if (i + 1 != num[j]) {
-        int temp = num[j];
-        num[j] = i + 1;
-        num[max[i]] = temp;
-        break;
-      }
-    }
-  }
-  */
 }
