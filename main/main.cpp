dcf2a870f861c666183aa879d5b0ba154319b644
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2025-05-14 06:54:13 +0000

    edit main

--- main/main.cpp
+++ main/main.cpp
@@ -1,52 +1,26 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-
-int arr[500001];
-map<int, int> big_map;
-
-int selectionsort(int size, int swap) {
-  int cnt = 0;
-  int idx = size - 1;
-
-  for (auto it = big_map.rbegin(); it != big_map.rend(); it++) {
-    int big = it->second;
-
-    if (big != idx) {
-      auto temp = arr[idx];
-      arr[idx] = it->first;
-      arr[big] = temp;
-
-      it->second = idx;
-      auto find = big_map.find(temp);
-      find->second = big;
-
-      cnt++;
-
-      if (cnt == swap) {
-        cout << arr[big] << ' ' << arr[idx] << '\n';
-        return 0;
-      }
-
-      idx--;
-    }
-  }
-  cout << -1 << '\n';
-  return 0;
-}
-
 int main() {
-  int size, swap;
-  cin >> size >> swap;
-  for (int i = 0; i < size; i++) {
-    int num;
-    cin >> num;
-    arr[i] = num;
-    big_map[num] = i;
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < (n * 2) - (i * 2) - 1; j++) {
+      cout << '*';
+    }
+    cout << '\n';
   }
-
-  selectionsort(size,swap);
-
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 2; j++) {
+      cout << ' ';
+    }
+    for (int j = 0; j < i * 2 + 3; j++) {
+      cout << '*';
+    }
+    cout << '\n';
+  }
   return 0;
 }
