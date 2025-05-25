50975c53148b6e917f4ab4ebd64340427326a472
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2025-05-25 06:49:37 +0000

    edit main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -1,26 +1,55 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
+#include <vector>
 using namespace std;
 
+void inputArray(vector<int>& arr, size_t size, map<int, int>& myMap, bool map) {
+  arr.resize(size);  // size에 맞게 벡터 크기 재할당
+  if (map) {
+    for (int i = 0; i < size; i++) {
+      cin >> arr[i];
+      myMap[arr[i]] = i;
+    }
+  } else {
+    for (size_t i = 0; i < size; i++) cin >> arr[i];
+  }
+}
+
+int selectionSort(vector<int>& arr1, vector<int>& arr2, size_t size,
+                  map<int, int> myMap) {
+  if (arr1 == arr2) return 1;
+  size_t index = size - 1;
+
+  for (auto i = myMap.rbegin(); i != myMap.rend(); i++) {
+    size_t big = i->second;
+
+    if (big != index) {
+      int temp = arr1[index];
+      arr1[index] = i->first;
+      arr1[big] = temp;
+
+      i->second = index;
+      auto find = myMap.find(temp);
+      find->second = big;
+    }
+
+    if (arr1 == arr2) return 1;
+    index--;
+  }
+  return 0;
+}
+
 int main() {
-  int n;
-  cin >> n;
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < i; j++) {
-      cout << ' ';
-    }
-    for (int j = 0; j < (n * 2) - (i * 2) - 1; j++) {
-      cout << '*';
-    }
-    cout << '\n';
-  }
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - i - 2; j++) {
-      cout << ' ';
-    }
-    for (int j = 0; j < i * 2 + 3; j++) {
-      cout << '*';
-    }
-    cout << '\n';
-  }
+  vector<int> arr1, arr2;
+  map<int, int> myMap;
+  size_t size;
+  cin >> size;
+
+  inputArray(arr1, size, myMap, true);
+  inputArray(arr2, size, myMap, false);
+
+  int isSame = selectionSort(arr1, arr2, size, myMap);
+  cout << isSame << '\n';
   return 0;
 }
