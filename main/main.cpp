3b637eb4ff7bfb138948f9b1e3830914d53a7bba
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2025-05-27 10:36:52 +0000

    edit main

--- main/main.cpp
+++ main/main.cpp
@@ -1,18 +1,55 @@
+#include <algorithm>
 #include <iostream>
-#include <unordered_map>
+#include <map>
 #include <vector>
 using namespace std;
 
-int main() {
-  unordered_map<int, int> myMap;
-  vector<int> arr(10);
-  for (int i = 0; i < 5; i++) {
-    cin >> arr[i];
-    myMap[arr[i]] = i;
+void inputArray(vector<int>& arr, size_t size, map<int, int>& myMap, bool map) {
+  arr.resize(size);  // size에 맞게 벡터 크기 재할당
+  if (map) {
+    for (int i = 0; i < size; i++) {
+      cin >> arr[i];
+      myMap[arr[i]] = i;
+    }
+  } else {
+    for (size_t i = 0; i < size; i++) cin >> arr[i];
   }
+}
 
-  for (auto i = myMap.begin(); i != myMap.end(); i++) {
-    cout << i->first << ' ' << i->second << '\n';
+int selectionSort(vector<int>& arr1, vector<int>& arr2, size_t size,
+                  map<int, int> myMap) {
+  if (arr1 == arr2) return 1;
+  size_t index = size - 1;
+
+  for (auto i = myMap.rbegin(); i != myMap.rend(); i++) {
+    size_t big = i->second;
+
+    if (big != index) {
+      int temp = arr1[index];
+      arr1[index] = i->first;
+      arr1[big] = temp;
+
+      i->second = index;
+      auto find = myMap.find(temp);
+      find->second = big;
+    }
+
+    if (arr1 == arr2) return 1;
+    index--;
   }
+  return 0;
+}
+
+int main() {
+  vector<int> arr1, arr2;
+  map<int, int> myMap;
+  size_t size;
+  cin >> size;
+
+  inputArray(arr1, size, myMap, true);
+  inputArray(arr2, size, myMap, false);
+
+  int isSame = selectionSort(arr1, arr2, size, myMap);
+  cout << isSame << '\n';
   return 0;
 }
