ced79115e5acab728382b7075ead7356a7e96551
LeeDoyun <134493573+CodingKingDoyun@users.noreply.github.com>
2025-08-31 06:15:04 +0000

    Add Rust devcontainer

--- main/main.cpp
+++ main/main.cpp
@@ -1,55 +1,19 @@
-#include <algorithm>
 #include <iostream>
-#include <map>
+#include <string>
 #include <vector>
-using namespace std;
-
-void inputArray(vector<int>& arr, size_t size, map<int, int>& myMap, bool map) {
-  arr.resize(size);  // size에 맞게 벡터 크기 재할당
-  if (map) {
-    for (int i = 0; i < size; i++) {
-      cin >> arr[i];
-      myMap[arr[i]] = i;
-    }
-  } else {
-    for (size_t i = 0; i < size; i++) cin >> arr[i];
-  }
-}
-
-int selectionSort(vector<int>& arr1, vector<int>& arr2, size_t size,
-                  map<int, int> myMap) {
-  if (arr1 == arr2) return 1;
-  size_t index = size - 1;
-
-  for (auto i = myMap.rbegin(); i != myMap.rend(); i++) {
-    size_t big = i->second;
-
-    if (big != index) {
-      int temp = arr1[index];
-      arr1[index] = i->first;
-      arr1[big] = temp;
-
-      i->second = index;
-      auto find = myMap.find(temp);
-      find->second = big;
-    }
-
-    if (arr1 == arr2) return 1;
-    index--;
-  }
-  return 0;
-}
 
 int main() {
-  vector<int> arr1, arr2;
-  map<int, int> myMap;
-  size_t size;
-  cin >> size;
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-  inputArray(arr1, size, myMap, true);
-  inputArray(arr2, size, myMap, false);
+  std::string s;
+  std::cin >> s;
 
-  int isSame = selectionSort(arr1, arr2, size, myMap);
-  cout << isSame << '\n';
+  std::vector<int> list(26);
+  for (int i = 0; i < s.length(); i++) list[s[i] - 97]++;
+
+  for (int i = 0; i < list.size(); i++)
+    std::cout << list[i] << (i != list.size() - 1 ? ' ' : '\n');
+
   return 0;
 }
